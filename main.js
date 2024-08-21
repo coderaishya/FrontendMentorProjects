f87d0d8bf29ec42392b70e832544d6cf6ddd858f
Aishwarya Dhuri <Aishwarya.Dhuri@asl.mum.sst>
2024-08-21 09:55:39 +0530

    Main project index page added

---
+++ main.js
@@ -0,0 +1,39 @@
+const projects = [
+	{
+		name: 'faq-accordin',
+		github:
+			'https://github.com/coderaishya/FrontendMentorProjects/tree/master/faq-accordin'
+	},
+   
+	
+];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name, github }, i) => {
+	const listItem = document.createElement('li');
+
+	listItem.innerHTML = `
+		<a href="${name}/index.html">
+			<img src="${name}/design/desktop-design.jpg" alt="${name}" />
+			<p>${i + 1}. ${formatProjectName(name)}</p>
+		</a>
+		<div class="links-container">
+			<a href="./${name}/index.html" class="blue">
+				<i class="fas fa-eye"></i>
+			</a>
+			<a href="${github}" class="github">
+				<i class="fab fa-github"></i>
+			</a>
+		</div>
+	`;
+
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
